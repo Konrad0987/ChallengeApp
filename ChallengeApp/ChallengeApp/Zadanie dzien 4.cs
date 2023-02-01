d676c2989d7a0d490417499d0e30844aefe1c714
Konrad0987 <szubzdak@gmail.com>
2023-02-01 16:59:42 +0100

    Zadanie dzien 4

---
+++ ChallengeApp/ChallengeApp/Zadanie dzien 4.cs
@@ -0,0 +1,31 @@
+﻿/// Zadanie dzień 4
+string personName = "Ewa";
+string gender = "woman";
+uint personAge = 33;
+Console.WriteLine("name - " + personName);
+Console.WriteLine("gender - " + gender);
+Console.WriteLine("person age - " + personAge);
+if (gender == "woman" && personAge < 30)
+{
+    Console.WriteLine("Kobieta poniżej 30 lat");
+}
+else
+{
+    Console.WriteLine("Kobieta nie znaleziona");
+}
+if (gender == "woman" && personName == "Ewa" && personAge == 33)
+{
+    Console.WriteLine("Ewa lat 33");
+}
+else
+{
+    Console.WriteLine("Ewa nie znaleziona");
+}
+if (gender == "man" && personAge < 18)
+{
+    Console.WriteLine("Niepełnoletny mążczyzna");
+}
+else
+{
+    Console.WriteLine("Mężczyzna nie znaleziony");
+}
